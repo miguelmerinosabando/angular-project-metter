67f803f9895b2e6cd127aed4b275d9f2fd292d1e
Miguel Merino <mig.merino3@gmail.com>
2018-05-26 21:18:39 +0200

    Inicio App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+//Importamos MDBootstrap 
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+import { AppRoutingModule } from './/app-routing.module';
+//Importamos componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    AppRoutingModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
